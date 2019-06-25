91612458037469465621401cbd5c1e9ec6cf381a
dac <dac@dypm.org>
2019-06-25 22:17:03 +0530

    Signed-off-by: dac <dac@dypm.org>

--- express.js
+++ express.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
  
-app.listen(3000)
+app.listen(3000);
+console.log("Server is running on 3000");
